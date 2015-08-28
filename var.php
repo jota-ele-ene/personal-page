708bf828ecfe5a6fb50a6da2c683c073826ae9ba
jota-ele-ene <jota-ele-ene@users.noreply.github.com>
2015-08-28 14:52:53 +0200

    Adding GitHub profile to var.php

--- var.php
+++ var.php
@@ -26,6 +26,7 @@ $profiles = array
   array("Twitter timeline","https://twitter.com/jota_ele_ene",'<i class="fa fa-twitter fa-2" style="font-size: x-large;margin-top: 0.4em;"></i>'),
   array("Klout profile","http://klout.com/#/jota_ele_ene",'<span style="font-size: 1.6em;font-weight: bold;">K</span>'),
   array("Medium page","https://medium.com/@jota_ele_ene",'<img src="icons/M.gif" style="height: 20px;margin: 10px 5px;">'),
+  array("GitHub page","https://github.com/jota-ele-ene",'<i class="fa fa-github fa-2" style="font-size: x-large;margin-top: 0.4em;"></i>'),
   array("Clarity page","https://clarity.fm/jota.ele.ene",'<img src="icons/C.gif" style="height: 20px;margin: 10px 5px;">'),
   );
 
