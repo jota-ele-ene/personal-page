527214d4695ccc2e5ea992635180b555a40c1a82
jota-ele-ene <jota-ele-ene@users.noreply.github.com>
2016-09-02 12:46:04 +0200

    Refactoring update.php

--- setup/custom.php
+++ setup/custom.php
@@ -1,35 +1,53 @@
 <?php
 
-//mysql db connection information
-$hostname = "127.0.0.1"; //your db host 
-$database = "shortener"; //your database name
-$username = "shortener"; //username for your database
-$password = "bla"; //password for your database
+//******************************************************
+//  Variables needed for customizing the home page style
+//******************************************************
 
 //My site page contents
 $title = "Are you looking for me?";
 $text = "Hello world!";
 
+//Choose the colors you like to customize the final page
 $bubbles = 'YES';
 $bubblesColor1 = '#FFFF00';
 $bubblesColor2 = '#AAAAAA';
 $bubblesColor3 = '#444444'; 
 
-//Profiles to link
+//Profiles to link from the page
 $profiles = array
   (
-  // Each profile consists of alt,href,html
-  array("Twitter timeline","https://twitter.com/jota_ele_ene",'<i class="fa fa-twitter fa-2" style="font-size: x-large;margin-top:8px;"></i>'),
-//  array("Medium page","https://medium.com/@jota_ele_ene",'<img src="icons/M.gif" style="height: 20px;margin: 10px 5px;">'),
-  array("Medium page","https://medium.com/@jota_ele_ene",'<i class="fa fa-medium fa-2" style="font-size: x-large;margin-top:8px;"></i>'),
-  array("GitHub page","https://github.com/jota-ele-ene",'<i class="fa fa-github fa-2" style="font-size: x-large;margin-top:8px;"></i>'),
-  array("Linkedin profile","https://es.linkedin.com/in/jotaeleene",'<i class="fa fa-linkedin fa-2" style="font-size: x-large;margin-top:8px;"></i>'),
-  array("Bitcoin Address","https://blockchain.info/es/address/1BUTbrpKTo3f4P2SmkWqQooZxptRysobjr",'<i class="fa fa-btc fa-2" style="font-size: x-large;margin-top: 8px;"></i>'),
-  array("PGP Key","https://keybase.io/jota_ele_ene/key.asc",'<i class="fa fa-key fa-2" style="font-size: x-large;margin-top: 8px;"></i>'),
+  // Each profile consists of alt,href,html. Insert as many profiles and you want. The icon is rendered from FontAwesome
+  array("Twitter timeline","<insert here your own Twitter timeline URL>",'<i class="fa fa-twitter fa-2" style="font-size: x-large;margin-top: 0.4em;"></i>'),
+  array("Medium page","<insert here your own Twitter timeline URL>",'<i class="fa fa-medium fa-2" style="font-size: x-large;margin-top: 0.4em;"></i>'),
+  array("GitHub page","<insert here your own Github profile>",'<i class="fa fa-github fa-2" style="font-size: x-large;margin-top: 0.4em;"></i>'),
   );
 
 // Keywords for retrieving Unsplash pictures  
-$keywords = array ("fruits", "nature", "city", "night", "people", "sea", "beach","mountain","water");
+$keywords = array ("fruits", "nature", "city", "night", "people", "sea", "beach","birds","desert");
+
+//$muzak = <insert here an audio file>;
+
+//Features to run if command
+//$features = array
+//  (
+//   'short' => 'shortener.php',
+//   'upload' => 'upload.php',
+//  );
+
+//*****************************************************
+//  Variables needed for enabling the shortener feature
+//*****************************************************
+
+//Timezone for handle dates
+$timezone = 'Europe/Madrid'; 
+
+//mysql db connection information - this is only need for the shortener - update with your own data
+$hostname = "127.0.0.1"; //your db host 
+$database = "shortener"; //your database name
+$username = "shortener"; //username for your database
+$password = "00short00"; //password for your database
+
 
 
 ?>
